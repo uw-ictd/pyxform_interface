6d8f970c508a80f015bbbc4e24fecd547a674a68
Nathan Breit <nabreit@gmail.com>
2012-02-22 15:12:34 -0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 from django.views.generic.simple import direct_to_template
+import os
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -10,9 +11,7 @@ urlpatterns = patterns('',
 
     (r'^tmp/(?P<path>.*)$', 'views.serve_xform'),
 
-    (r'^css/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.CSS_ROOT
-    }),
+    (r'^css/(?P<path>.*)$', 'django.views.static.serve'),
 
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.MEDIA_ROOT
