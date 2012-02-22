25d9a7382d23d464e1f139c49fb2fa941aa7488c
Nathan Breit <nabreit@gmail.com>
2012-02-22 13:37:08 -0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,17 +1,24 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from django.conf import settings
+from django.views.generic.simple import direct_to_template
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pyxform.views.home', name='home'),
-    # url(r'^pyxform/', include('pyxform.foo.urls')),
+    url(r'^$', 'xlsform.views.index'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^tmp/(?P<path>.*)$', 'views.serve_xform'),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    (r'^css/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.CSS_ROOT
+    }),
+
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT
+    }),
+
+    (r'^doc/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.DOC_ROOT
+    }),
 )
