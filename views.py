92669bde43474a9ada5510794cfeefc00e1b262b
Nathan Breit <nabreit@gmail.com>
2013-03-22 15:24:38 -0700

    Getting rid of hardcoded server URL.

--- views.py
+++ views.py
@@ -79,6 +79,7 @@ def index(request):
             return render_to_response('upload.html', {
                 'form': UploadFileForm(),
                 'xml_path' : '.' + xml_path,
+                'xml_url' : request.build_absolute_uri(xml_path),
                 'success': not error,
                 'error': error,
                 'warnings': warnings,
